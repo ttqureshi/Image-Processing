834b3e9d95dd39dac191d7f43b849c249c8beb76
Muhammad Tayyab Tahir Qureshi <m.tayyabtq@gmail.com>
2023-10-19 14:23:27 +0500

    cleaning stuff

--- Image Processing/sobelEdgeDetector.py
+++ Image Processing/sobelEdgeDetector.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2 as cv
 import utils
 
-image = cv.imread('imgs/person.jpg')
+image = cv.imread('imgs/einstein.jpg')
 cv.imshow('Original Image', image)
 image_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 cv.imshow('Grayscale',image_gray)
@@ -31,6 +31,15 @@ cv.imshow('Edges',edges)
 
 noisy_edges = utils.gradient(noisy_image_gray)
 cv.imshow('Edges of noisy image',noisy_edges)
+
+
+# Saving results
+cv.imwrite('imgs/sobel_edge_detector_results/1_original_image.jpg', image)
+cv.imwrite('imgs/sobel_edge_detector_results/2_gray_scaled.jpg', image_gray)
+cv.imwrite('imgs/sobel_edge_detector_results/3_noisy_image.jpg', noisy_image_gray)
+cv.imwrite('imgs/sobel_edge_detector_results/4_weighted_smoothing.jpg', weighted_smooth)
+cv.imwrite('imgs/sobel_edge_detector_results/5_noisy_image_edges.jpg', noisy_edges)
+cv.imwrite('imgs/sobel_edge_detector_results/6_edges_smooth.jpg', edges)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
